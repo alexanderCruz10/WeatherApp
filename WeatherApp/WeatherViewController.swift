d697357b956a5b441ef72adfdcac80d1663be57f
alexandercruz <alexandercruz1094@gmail.com>
2021-10-05 22:22:34 -0400

    did setup

---
+++ WeatherApp/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Alex Cruz on 2021-10-02.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func addLocationDidTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func locationDidTapped(_ sender: Any) {
+        
+    }
+
+}
+
